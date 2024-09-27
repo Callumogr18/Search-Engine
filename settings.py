e30d1630912220345edfb5f2c644b320398a5ec5
Callumogr18 <callum.ryan.2702@gmail.com>
2024-09-28 00:12:44 +0100

    Changed country search

--- settings.py
+++ settings.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 SEARCH_KEY = os.getenv('SEARCH_KEY')
 SEARCH_ID = os.getenv('SEARCH_ID')
-COUNTRY = "us"
+COUNTRY = "ie"
 SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&num=10&gl=" + COUNTRY
 RESULT_COUNT = 20
